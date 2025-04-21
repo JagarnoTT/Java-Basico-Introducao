67c81fb0cd9c79afc22a12d0f2a11e7f395e9ffe
Ericles Roque <jagarnotprev@gmail.com>
2025-04-21 18:19:48 -0300

    Alteração

--- Tipos-Variaveis/src/TiposVariaveis.java
+++ Tipos-Variaveis/src/TiposVariaveis.java
@@ -1,5 +1,16 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        byte idade = 27;
+        short ano = 2021;
+        int cep = 89910090;
+        long cpf = 89944503509L;
+        float pi = 3.14F;
+        double salario = 2500.50;
+        String nome = "Ericles";
+        char sexo = 'M';
+        double peso = 1.72;
+        boolean estudante = true;
+         final double imposto = 0.15;
+
     }
 }
