22ef84f22ec5b5939d07b99c182d3e8d79dc6f48
Ericles Roque <jagarnotprev@gmail.com>
2025-05-03 22:05:53 -0300

    alteração

--- metodos/src/condiçãoSimples.java
+++ metodos/src/condiçãoSimples.java
@@ -1,8 +1,8 @@
 public class condiçãoSimples {
     public static void main(String[] args) {
-        int nota = 4;
+        int nota = 6;
 
-        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota <7 ? "Recuperação" : "Reprovado";
+        String resultado = nota >= 7 ? "Aprovado, sua nota foi: " + nota: nota >= 5 && nota <7 ? "Recuperação, sua nota foi: "+nota : "Reprovado, sua nota foi: "+nota;
         System.out.println(resultado);
     }
 }
