1fdfc0d45d72d8146e566359d87595cede6dd2a7
Ericles Roque <jagarnotprev@gmail.com>
2025-05-03 22:08:22 -0300

    alteração

---
+++ metodos/src/switchCase.java
@@ -0,0 +1,20 @@
+public class switchCase {
+    public static void main(String[] args) {
+        int number = 5;
+
+        switch (number){
+            case 1:
+            System.out.println("Number is 1");
+                break;
+            case 2:
+            System.out.println("Number is 2");
+                break;
+            case 3:
+            System.out.println("Number is 3");
+                break;
+            default:
+            System.out.println("Number is not 1, 2 or 3");
+                break;
+        }
+    }
+}
