fbeb8604c72cf2ba021eb6c6b47f12310c8d9138
Ericles Roque <jagarnotprev@gmail.com>
2025-05-08 19:59:43 -0300

    EstruturaFor

---
+++ Estruturas de repetição/estruturasDeRepetição/src/estruturaFor.java
@@ -0,0 +1,7 @@
+public class estruturaFor{
+    public static void main(String[] args) {
+        for (int carneirinhos = 0; carneirinhos <= 20; carneirinhos++){
+            System.out.println("Carneirinhos: " + carneirinhos);
+        }
+    }
+}
