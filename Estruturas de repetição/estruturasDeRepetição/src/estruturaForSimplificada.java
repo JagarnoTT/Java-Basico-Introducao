e46d3df2424e96ace539b2b1d804cdcdce656c3b
Ericles Roque <jagarnotprev@gmail.com>
2025-05-08 20:24:07 -0300

    estruturaForSimplificada

---
+++ Estruturas de repetição/estruturasDeRepetição/src/estruturaForSimplificada.java
@@ -0,0 +1,9 @@
+public class estruturaForSimplificada {
+    public static void main(String[] args) {
+        String alunos [] = { "João", "Maria", "Pedro", "Ana", "Carlos" };
+        
+        for (String estudantes : alunos){
+            System.out.println("Os alunos são : " + estudantes);
+        }
+    }    
+}
