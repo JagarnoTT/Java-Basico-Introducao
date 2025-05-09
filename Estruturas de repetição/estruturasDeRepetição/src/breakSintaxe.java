4df21a40e052e5fda659c3a536f89a49a933e3cf
Ericles Roque <jagarnotprev@gmail.com>
2025-05-08 21:47:55 -0300

    breakContinueSinxtaxe

---
+++ Estruturas de repetição/estruturasDeRepetição/src/breakSintaxe.java
@@ -0,0 +1,10 @@
+public class breakSintaxe {
+    public static void main(String[] args) {
+        for (int number = 1; number <= 5; number++){
+            if (number == 3){
+                break;
+            }
+            System.out.println(number);
+        }
+    }
+}
