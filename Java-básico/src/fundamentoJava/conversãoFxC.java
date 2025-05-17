6451d16bd703de5349c57bece99218836bcb7d5b
Ericles Roque <jagarnotprev@gmail.com>
2025-05-17 19:00:41 -0300

    alteração

---
+++ Java-básico/src/fundamentoJava/conversãoFxC.java
@@ -0,0 +1,10 @@
+package fundamentoJava;
+
+public class conversãoFxC {
+	public static void main(String [] args) {
+		double F = 68.0;
+		double C = (F - 32) / 1.8;
+		
+		System.out.println("O valor de "+ F+"º Fahrenheit convertido em Celsius é: "+C+"°C");
+	}
+}
